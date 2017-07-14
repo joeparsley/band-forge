080116d4469dca92dbc1d9e18656711a808d840b
Joe Parsley <joewparsley@gmail.com>
2017-07-14 11:29:10 -0700

    add bands model

---
+++ src/app/bands.model.ts
@@ -0,0 +1,4 @@
+export class Bands {
+
+  constructor(public title: string, public description: string, public members: string, public guitar: string, public bass: string, public drums: string, public guitarTwo: string){ }
+}
